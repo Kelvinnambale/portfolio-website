68e0f3d564eece3b591454ac25890a947d758d54
Kelvinnambale <120830157+Kelvinnambale@users.noreply.github.com>
2024-01-22 11:46:57 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,80 @@
+let words = document.querySelectorAll(".word")
+words.forEach((word)=>{
+    let letters = word.textContent.split("");
+    word.textContent="";
+    letters.forEach((letter)=>{
+        let span = document.createElement("span");
+        span.textContent = letter;
+        span.className = "letter";
+        word.append(span);
+    });
+});
+
+let currentWordIndex = 0;
+let maxWordIndex = words.length -1;
+words[currentWordIndex].style.opacity = "1";
+
+let changeText = () =>{
+    let currentWord = words[currentWordIndex];
+    let nextWord = currentWordIndex === maxWordIndex ? words[0] : words[currentWordIndex + 1];
+
+    Array.from(currentWord.children).forEach((letter,i)=>{
+        setTimeout(()=>{
+            letter.className = "letter out";
+        },i * 80);
+    })
+    nextWord.style.opacity="1";
+    Array.from(nextWord.children).forEach((letter,i)=>{
+        letter.className = "letter behind";
+        setTimeout(()=>{
+            letter.className = "letter in";
+        },340 + i * 80);
+    });
+    currentWordIndex = currentWordIndex === maxWordIndex ? 0 : currentWordIndex + 1;
+};
+changeText();
+setInterval(changeText,3000);
+
+// ActiveMenu
+let menuli =document.querySelectorAll('header ul li a');
+let section=document.querySelectorAll('section')
+
+function activeMenu(){
+    let len = section.length;
+    while(--len && window.scrollY + 97 < section[len].offsetTop){}
+    menuli.forEach(sec => sec.classList.remove("active"));
+    menuli[len].classList.add("active");
+}
+
+activeMenu();
+window.addEventListener("scroll",activeMenu);
+
+// sticky navbar
+const header =document.querySelectorAll("header");
+window.addEventListener("scroll",function(){
+    header.classList.toggle("sticky",window.scrollY > 50)
+});
+// toggle icon navbar
+let menuIcon = document.querySelectorAll("#menu-icon");
+let navlist = document.querySelectorAll(".navlist");
+
+menuIcon.onclick =() => {
+    menuIcon.classList.toggle("bx-x");
+    navlist.classList.toggle("open");
+};
+
+window.onscroll =() => {
+    menuIcon.classList.remove("bx-x");
+    navlist.classList.remove("open");
+};
+
+//scroll reveal
+ScrollReveal({
+    reset: true,
+    distance: '80px',
+    duration: 2000,
+    delay: 200
+});
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top'});
+ScrollReveal().reveal('.home-image, .skills-container, .portfolio-box, .contact form', { origin: 'bottom'});
